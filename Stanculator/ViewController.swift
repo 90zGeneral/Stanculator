9a541c428deb8758d97deb188f49b2000f16d484
Roydon Jeffrey <brushdam14823@gmail.com>
2017-03-22 16:09:57 -0400

    Lecture 1 - Basic calculator demo with number digits and pie & square root operations.

--- Stanculator/ViewController.swift
+++ Stanculator/ViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    var userIsCurrentlyTyping = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //Number buttons
+    @IBAction func digitTouched(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsCurrentlyTyping {
+            let currentDisplayText = display.text!
+            display.text = currentDisplayText + digit
+        }else {
+            display.text = digit
+            userIsCurrentlyTyping = true
+        }
+    }
+    
+    //Getter and Setter
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
     }
 
-
+    //Math operation buttons
+    @IBAction func mathOperation(_ sender: UIButton) {
+        userIsCurrentlyTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "π":
+                displayValue = Double.pi
+            case "√":
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+        
+    }
 }
 
